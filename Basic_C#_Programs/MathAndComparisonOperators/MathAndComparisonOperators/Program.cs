22f3519bd2967b0300df6238a7c85e631dea1c8c
Pablo <psalazar5@live.com>
2023-04-20 17:44:00 -0400

    Operands

---
+++ Basic_C#_Programs/MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int total = 5 + 10;
+            //int othertotal = 12 + 22;
+            //int combined = total + othertotal;
+            //Console.WriteLine(combined); //Console.WriteLine("Five plus ten = " + total.ToString());
+            //Console.ReadLine();
+
+            ////subtraction
+            //int difference = 10 - 5;
+            //Console.WriteLine("ten minus 5  = " + difference.ToString());
+            //Console.ReadLine();
+
+            //Multiplication 
+            //int product = 43 * 49;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            ////Division
+            //int quotient = 294 / 2;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+            ////double quotient = 100.0 / 17.0; 
+            ////Console.WriteLine(quotient);
+            ////Console.ReadLine();
+
+            ////Remainder modulus operator 
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool TrueorFalse = 12 > 5;
+            //Console.WriteLine(TrueorFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 70;
+
+            //bool isWarm = currentTemperature >= roomTemperature;
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            Console.WriteLine("Please enter a number");
+            int yourNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(yourNumber * 50);
+          
+
+            Console.WriteLine("Please enter another number");
+            int anotherNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(anotherNumber + 25);
+
+            Console.WriteLine("Another");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number2 / 12.5);
+
+            Console.WriteLine("Again");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number3 <= 50);
+
+            Console.WriteLine("and again...");
+            int number4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number4 % 7);
+            
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
--- Basic_C#_Programs/VariableandDataTypes/VariableandDataTypes/Program.cs
+++ Basic_C#_Programs/VariableandDataTypes/VariableandDataTypes/Program.cs
@@ -57,47 +57,79 @@ namespace VariableandDataTypes
             //Console.WriteLine(rainingstatus);
             //Console.ReadLine();
 
-            Console.WriteLine("The Tech Academy\n" ); //Writeline of the tech academy 
-            Console.WriteLine("Student Daily Report"); //Daily Report Write line 
-            Console.WriteLine("What is your name?");
-            string yourName = Console.ReadLine(); //string for the response given 
-            Console.WriteLine("Awesome " + yourName + "!");
-       
-  
+            //Console.WriteLine("The Tech Academy\n"); //Writeline of the tech academy 
+            //Console.WriteLine("Student Daily Report"); //Daily Report Write line 
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine(); //string for the response given 
+            //Console.WriteLine("Awesome " + yourName + "!");
 
-            Console.WriteLine("What course are you on?");
-            string yourCourse = Console.ReadLine(); //readline reads the string user enters 
-            Console.WriteLine("Great!"); //This spits out the consoles response to this answer
-            
 
-            Console.WriteLine("What page number?");
-            string pageNumber = Console.ReadLine(); 
-            int urpageNumber = Convert.ToInt32(pageNumber); //converted the string into an int number 
-            Console.Write("Spectacular!\n");
-       
-            
-            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\" ");
-            string Answer = Console.ReadLine();
-            bool yourAnswer = Convert.ToBoolean(Answer);
-            Console.WriteLine("You answered " + Answer + " we will answer back soon.");
 
-            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
-            string experiences = Console.ReadLine(); //console readline exams the users response 
-            Console.WriteLine("Thank you for sharing your experience "); //spitting back out my response 
-            
+            //Console.WriteLine("What course are you on?");
+            //string yourCourse = Console.ReadLine(); //readline reads the string user enters 
+            //Console.WriteLine("Great!"); //This spits out the consoles response to this answer
 
-            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-            string feedback = Console.ReadLine();
-            Console.WriteLine("We will do everything in our power to keep you satisfied.");
-          
 
-            Console.WriteLine("How many hours did you study today?");
-            string numberHours = Console.ReadLine();
-            int yourHours = Convert.ToInt32(numberHours); //conver string to int 
-            Console.WriteLine("Your hours are our satisfaction, good job on " + yourHours + " hours today, thank you for your answers and an instructor will respond to\nyou shortly. Have a great day!");
-            Console.ReadLine(); //reads line and keeps responses and questions in the commaand 
+            //Console.WriteLine("What page number?");
+            //string pageNumber = Console.ReadLine();
+            //int urpageNumber = Convert.ToInt32(pageNumber); //converted the string into an int number 
+            //Console.Write("Spectacular!\n");
+            ////int urpageNumber = convert.ToInt32(console.readline()); this makes it into one line of code instead of 2 
 
-            
+            //Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\" ");
+            //string Answer = Console.ReadLine();
+            //bool yourAnswer = Convert.ToBoolean(Answer);
+            //Console.WriteLine("You answered" + Answer + " we will answer back soon.");
+            ////bool yourAnswer = convert.ToBoolean(Console.ReadLine());
+
+            //Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            //string experiences = Console.ReadLine(); //console readline exams the users response 
+            //Console.WriteLine("Thank you for sharing your experience "); //spitting back out my response 
+
+
+            //Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            //string feedback = Console.ReadLine();
+            //Console.WriteLine("We will do everything in our power to keep you satisfied.");
+
+
+            //Console.WriteLine("How many hours did you study today?");
+            //string numberHours = Console.ReadLine();
+            //int yourHours = Convert.ToInt32(numberHours); //conver string to int 
+            //Console.WriteLine("Your hours are our satisfaction, good job on " + yourHours + " hours today, thank you for your answers and an instructor will respond to\nyou shortly. Have a great day!");
+            //Console.ReadLine(); //reads line and keeps responses and questions in the commaand 
+            ////int yourHours = Convert.ToInt32(Console.ReadLine());
+           
+
+            //Addition total 
+            int num1 = 1827823; 
+            int num2 = 13749284;
+            int total = num1 + num2;
+            Console.WriteLine(total);
+
+            //Subtraction (difference)
+            int num3 = 379283;
+            int num4 = 29248;
+            int difference = num3 - num4;
+            Console.WriteLine(difference);
+
+            //Multiplication(product)
+            int num5 = 27739;
+            int num6 = 92974;
+            int product = num5 * num6;
+            Console.WriteLine(product);
+            //Or i can do console.writeline(num5 * num6);
+
+            //Division(quotient)
+            int num7 = 356288;
+            int num8 = 343;
+            int quotient = num7 / num8;
+            Console.WriteLine(quotient);
+
+            //Math operation on a string 
+            string name = "Luke";
+            int number = 10;
+            Console.WriteLine(name + number);
+            Console.ReadLine();
         }
     }
 }
