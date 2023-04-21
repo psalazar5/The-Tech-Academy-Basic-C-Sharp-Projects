354131c20f936318ebcf5f45127b72335c6d1986
Pablo <psalazar5@live.com>
2023-04-21 06:37:07 -0400

    BOOLEAN LOGIC SUBMISSION ASSIGNMENT

---
+++ Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            ////or 
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            ////==
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(false == false);
+            //Console.WriteLine(false == true);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+
+            ////XOR operator 
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false); //allows only one false and true for it to be true 
+            //Console.WriteLine(false ^ false); //only allows 1 to be true! 
+
+            //console.writeline(true && true && true || false);
+            //Console.WriteLine(false && true && true || false || false);
+
+
+            Console.WriteLine("What is your age?");
+            int yourAge = Convert.ToInt32(Console.ReadLine()); //converted string into into 
+
+            Console.WriteLine("Have you ever had a DUI? Please enter \"true\" or \"false\"");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingtickets = Convert.ToInt32(Console.ReadLine());
+
+            
+            bool qualification = (yourAge > 15 && dui == false && speedingtickets < 3); //Asked is your age greater than 15? & do you have a dui (false) == false (means tru) && do you have more than 3 speeding tickets?
+            Console.WriteLine("Qualified? \n" + qualification); //Wrote on console "Qualified" + the bool statement determining if false or true from the values assigned from the user 
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- Basic_C#_Programs/MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
+++ Basic_C#_Programs/MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -76,29 +76,68 @@ namespace MathAndComparisonOperators
             //Console.ReadLine(); //read line is at the end so that it doesnt get stuck on the command prompt 
 
 
-            Console.WriteLine("Anonymous Income Comparison Program");
-            Console.WriteLine("Person 1\nHourly Rate?"); //break line into new line and put hourly rate?
-            int hourlyrate = Convert.ToInt32(Console.ReadLine()); //converted this into one line instead of two 
-            Console.WriteLine("Hours worked per week?");
-            int hoursworked = Convert.ToInt32(Console.ReadLine()); //user needs to input their integer of hoursworked and this converts it into 
+            //Console.WriteLine("Anonymous Income Comparison Program");
+            //Console.WriteLine("Person 1\nHourly Rate?"); //break line into new line and put hourly rate?
+            //int hourlyrate = Convert.ToInt32(Console.ReadLine()); //converted this into one line instead of two 
+            //Console.WriteLine("Hours worked per week?");
+            //int hoursworked = Convert.ToInt32(Console.ReadLine()); //user needs to input their integer of hoursworked and this converts it into 
 
-            Console.WriteLine("Person 2\nHourly Rate?"); 
-            int hourlyrate2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Hours worked per week?");
-            int hoursworked2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Person 2\nHourly Rate?");
+            //int hourlyrate2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Hours worked per week?");
+            //int hoursworked2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Annual salary of Person 1:");
-            Console.WriteLine(hourlyrate * hoursworked * 52);
+            //Console.WriteLine("Annual salary of Person 1:");
+            //Console.WriteLine(hourlyrate * hoursworked * 52);
 
-            Console.WriteLine("Annual salary of Person 2:"); 
-            Console.WriteLine(hourlyrate2 * hoursworked2 * 52); //did math operation on both of these person1 and 2 and spit back out their salaries 
+            //Console.WriteLine("Annual salary of Person 2:");
+            //Console.WriteLine(hourlyrate2 * hoursworked2 * 52); //did math operation on both of these person1 and 2 and spit back out their salaries 
 
-            Console.WriteLine("Does person 1 make more money than Person 2?");
-            int Person1 = hourlyrate * hoursworked; //made person1 and person 2 in these lines 
-            int Person2 = hourlyrate2 * hoursworked2; //used their int of hourly rate and hoursworked and made a boolean statement out of it 
-            Console.WriteLine(Person1 >= Person2);
+            //Console.WriteLine("Does person 1 make more money than Person 2?");
+            //int Person1 = hourlyrate * hoursworked; //made person1 and person 2 in these lines 
+            //int Person2 = hourlyrate2 * hoursworked2; //used their int of hourly rate and hoursworked and made a boolean statement out of it 
+            //Console.WriteLine(Person1 >= Person2);
 
-            
+
+            ////&& boolean logic 
+            //int packagewidth = 10;
+            //int packageheight = 25;
+            //bool toobig = (packageheight > 25 && packagewidth > 25);
+            //Console.WriteLine(toobig); //^ this expression asks the question are both the package width & height greater than 25?
+
+            ////boolean logic (||) OR
+            //int packagewidth = 10;
+            //int packageheight = 25;
+            //bool toobig = (packageheight > 25 || packagewidth > 25);
+            //Console.WriteLine(toobig);
+
+            ////multiple uses of &&
+            //string fname = "Pablo";
+            //string lname = "Salazar";
+            //string ssn = "232-271-3723";
+            //bool istrue = (fname == "Pablo" && lname == "Salazar" && ssn == "232-271-3723");
+            //Console.WriteLine(istrue);
+            //Console.WriteLine(fname + " " + lname + " " + ssn);
+
+            ////boolean loogic multiple uses of or 
+            //int bankaccountbalance = 500;
+            //bool isinarockband = false;
+            //string name = "Jesse";
+
+            //bool isreallycool = (bankaccountbalance > 10000 || isinarockband || name == "Jesse");
+            //Console.WriteLine(isreallycool);
+
+            //int num1 = 15;
+            //int num2 = 12;
+            //int num3 = 4;
+            //int num4 = 38;
+            //int num5 = 23;
+            //int num6 = 1;
+            //int num7 = 4;
+
+            //bool result = (num1 > num2 && (num3 == num4 || num5 == num6));
+            //Console.WriteLine(result);
+            ////asks if num1 is greater than num2 and true or false, & either this or this true? , the output is false
 
             Console.ReadLine();
 
