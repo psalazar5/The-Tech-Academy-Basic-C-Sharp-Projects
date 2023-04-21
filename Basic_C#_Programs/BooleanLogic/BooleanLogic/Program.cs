b28608d28a71b7a41598f982b01f716c47197ed1
Pablo <psalazar5@live.com>
2023-04-21 17:32:48 -0400

    Branching submission statement

--- Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
+++ Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
@@ -35,22 +35,128 @@ namespace BooleanLogic
             //Console.WriteLine(true ^ false); //allows only one false and true for it to be true 
             //Console.WriteLine(false ^ false); //only allows 1 to be true! 
 
-            //console.writeline(true && true && true || false);
-            //Console.WriteLine(false && true && true || false || false);
+            //Console.WriteLine( true && true && false);
+            //Console.WriteLine(false && true && false || true);
+            //Console.WriteLine(3 > 5 && 5 > 2 && 5 > 10|| 10 > 6);
+            //Console.WriteLine(5 > 2 && 4 > 2 || 5 > 10);
 
 
-            Console.WriteLine("What is your age?");
-            int yourAge = Convert.ToInt32(Console.ReadLine()); //converted string into into 
+            //Console.WriteLine("What is your age?");
+            //int yourAge = Convert.ToInt32(Console.ReadLine()); //converted string into into 
 
-            Console.WriteLine("Have you ever had a DUI? Please enter \"true\" or \"false\"");
-            bool dui = Convert.ToBoolean(Console.ReadLine());
+            //Console.WriteLine("Have you ever had a DUI? Please enter \"true\" or \"false\"");
+            //bool dui = Convert.ToBoolean(Console.ReadLine());
 
-            Console.WriteLine("How many speeding tickets do you have?");
-            int speedingtickets = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("How many speeding tickets do you have?");
+            //int speedingtickets = Convert.ToInt32(Console.ReadLine());
 
-            
-            bool qualification = (yourAge > 15 && dui == false && speedingtickets < 3); //Asked is your age greater than 15? & do you have a dui (false) == false (means tru) && do you have more than 3 speeding tickets?
-            Console.WriteLine("Qualified? \n" + qualification); //Wrote on console "Qualified" + the bool statement determining if false or true from the values assigned from the user 
+
+            //bool qualification = (yourAge > 15 && dui == false && speedingtickets < 3); //Asked is your age greater than 15? & do you have a dui (false) == false (means tru) && do you have more than 3 speeding tickets?
+            //Console.WriteLine("Qualified? \n" + qualification); //Wrote on console "Qualified" + the bool statement determining if false or true from the values assigned from the user 
+
+
+            ////if/else statements
+            //string name = "Pablo";
+
+
+            //if (name == "Pablo")
+            //{
+            //    Console.WriteLine("Your name is Pablo");
+            //}
+            //else if (name5 == "Brett")
+            //{
+            //    Console.WriteLine("Your name is not Pablo");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your name is not Pablo. Your name is not Brett");
+            //}
+
+
+            //string yourname = "Brett";
+
+            //if (yourname == "Brett")
+            //{
+            //    Console.WriteLine("Your name is Brett.");
+            //}
+            //else if (yourname == "Pedro")
+            //{
+            //    Console.WriteLine("Your name is not Brett but hello Pedro.");
+            //}
+
+
+            ////if not statement 
+            //string myname = "Jake";
+            //if (myname != "Jake")
+            //{
+            //    Console.WriteLine("Clearly your name is not Jake!");
+            //}
+            //else if (name == "Pablo")
+            //{
+            //    Console.WriteLine("Good job Pablo!");
+            //}
+
+
+            ////Lesser than op 
+            //int age = 20;
+            //if (age < 21)
+            //{
+            //    Console.WriteLine("You are not old enough to buy the bottle.");
+            //}
+            //else if (age > 21)
+            //{
+            //    Console.WriteLine("The bottle will be $25");
+            //}
+
+            //int yourage = 21;
+
+            //if (yourage <= 25)
+            //{
+            //    Console.WriteLine("You are not able to rent the car");
+            //}
+            //else if (yourage >= 26) //More than or equal to 26
+            //{
+            //    Console.WriteLine("You are able to rent out the car.");
+            //}
+
+            //////And operator 
+            //int num1 = 7;
+            //if (true && true && true && true && num1 == 20)
+            //{
+            //    Console.WriteLine("everything was true");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("there was a false somewhere ");
+            //}
+
+
+            ////Or operator 
+            //int num5 = 7;
+            //if (num5 < 20 || num1 == 12) 
+            //{
+            //    Console.WriteLine("Atleast one value is true.");
+            //}
+            //if (num5 == 12 || num1 == 18)
+            //{
+            //    Console.WriteLine("Atleast one value is true.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No value is true");
+            //}
+            //if (num5 == 12 || num5 == 18 || num5 == 20 || num5 == 7)
+            //{
+            //    Console.WriteLine("Something is true");
+            //}
+
+
+            //Ternary operator 
+            int number1 = 7;
+            int number2 = 12;
+
+            string result = number1 > number2 ? "number 1 is greater than number 2" : "number 1 is not greater than number2";
+            Console.WriteLine(result);
 
 
             Console.ReadLine();
---
+++ Basic_C#_Programs/Branching/Branching/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("What is your fav num?");
+            //int favnum = Convert.ToInt32(Console.ReadLine());
+
+            //string results = favnum == 12 ? "You have a fav num" : "You do not have a fav num";
+            //Console.WriteLine(results);
+
+
+
+            //int roomtemperature = 70;
+
+            //Console.WriteLine("Hi, what is your name");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi " + name + " what is the temperature where you are?");
+            //int currenttemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currenttemperature == roomtemperature)
+            //{
+            //    Console.Write("It is exactly room temp");
+            //}
+            //else if (currenttemperature > roomtemperature)
+            //{
+            //    Console.Write("it is warmer than room temp");
+            //}
+            //else if (roomtemperature > currenttemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temp");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhhh... something went wrong");
+            //}
+
+
+
+
+            //int currenttemperature = 80;
+            //int roomtemperature = 70;
+
+
+            ////Terinary operator
+            //string comparisonresult = currenttemperature == roomtemperature ? "It is exactly room temp" : "It is not room temp";
+            //Console.WriteLine(comparisonresult);
+
+
+            //if (currenttemperature == roomtemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature");
+            //}
+            //else if (currenttemperature > roomtemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temp. ");
+            //}
+            //else if (roomtemperature > currenttemperature)
+            //{
+            //    Console.WriteLine("Room temp is warmer than current temp");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This is not exactly room temperature");
+            //}
+
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+            Console.WriteLine("Please enter the package weight:");
+            int packageweight = Convert.ToInt32(Console.ReadLine());
+            if (packageweight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else 
+            {
+                Console.WriteLine("Please enter package width: ");
+                int packagewidth = Convert.ToInt32(Console.ReadLine()); //enters width and converts
+
+                Console.WriteLine("Please enter package height : "); //enters height and converts
+                int packageheight = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter package length : "); //enters length and converts
+                int packagelength = Convert.ToInt32(Console.ReadLine());
+
+                int packagedimensions = packagewidth + packageheight + packagelength; //makes an int to add all dimensions
+                
+                if (packagedimensions > 50) //grabs those dimensions and makes statement if these dimensions are larger than 50 run this if 
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+                //else if (packagedimensions == 45)
+                //{
+                //    Console.WriteLine("Goodluck");
+                //    Console.ReadLine();
+                //    Environment.Exit(0);
+                //}
+                else //if they are not larger give them this quote i created below and a thankyou 
+                {
+                    int quote = (packageheight * packagewidth * packagelength) * packageweight / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quote + "\nThankyou");
+                    
+                }
+            }
+           
+
+            
+            Console.ReadLine();
+
+        }
+    }
+}
