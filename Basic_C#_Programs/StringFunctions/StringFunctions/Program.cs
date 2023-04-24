80124f48d739851adde661c96d9e49778304f65f
Pablo <psalazar5@live.com>
2023-04-24 12:47:32 -0400

    String assignment

---
+++ Basic_C#_Programs/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string name = "Jesse";
+            //string quote = "The man said, \\hello\", Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = @"C:\Users\psala"; //@ means everything is part of the string even the backslash otherwise with no @ u need double backslashes.
+
+            //bool trueOrfalse = name.Contains("s"); //asking is the string name has the letter 's' ... Is that true or false on the bool 
+            //trueOrfalse = name.EndsWith("s"); //re assigning it here.. this is how you re assign name it the same string and enter its value onces again.. Good when parsing files.
+
+            //int length = name.Length; //Property of a string.. grabbing the length of the string called 'name' up above
+            //name = name.ToUpper(); //converts everything to upper case within name
+            //name = name.ToLower(); //converts everything to lower case within name
+
+            ////inmutable string means once created you cannot change them, allocated to memory.
+
+
+            //StringBuilder urmom = new StringBuilder();
+
+            //urmom.Append("Urmom");
+
+            string line1 = "Pablo is ";
+            string line2 = "an awesome person who";
+            string line3 = " loves to workout and talk with friends.";
+
+            line1 = line1.ToUpper();
+
+            StringBuilder sp = new StringBuilder();
+            sp.Append(line1);
+            sp.Append(line2);
+            sp.AppendLine(line3);
+
+            Console.WriteLine(sp);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
