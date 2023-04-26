c37d3cb1110c450b38c7952beaff7a06564349a8
Pablo <psalazar5@live.com>
2023-04-26 16:23:03 -0400

    MethodSubmission

---
+++ Basic_C#_Programs/MethodSubmissionAssignment/MethodSubmissionAssignment/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Car
+    {
+        public int myMethod(int num1, int num2 = 10)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Car car = new Car(); // instantiation
+            Console.WriteLine("Please enter one number:"); //asks the user to input a number
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            try //then tries to enter another number and if they dont it goes into the catch 
+            {
+                Console.WriteLine("Please enter another number if you want too: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(car.myMethod(num1, num2)); //grabs the car my method from car class and grabs num1 and num2 optionally 
+            }
+            catch //if try fails then car my method will take in only num1 and optionally adds 10 to it (num2)
+            {
+                Console.WriteLine(car.myMethod(num1)); //calls in the method in the class, if no one puts in a number (10)
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
