778fd67919523ee6167db871891e9656c515d8c7
Pablo <psalazar5@live.com>
2023-04-26 15:48:00 -0400

    MainMethodAssignment

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Car
+    {
+        public int myMathOperation(int number)  //this method takes an integer does math and returns it  
+        {
+             return(number * 20);
+        }
+        public int myMathOperation(decimal number) //this method takes in a decimal does math and returns an integer 
+        {
+            return (Convert.ToInt32(number) + 20);
+        }
+        public int myMathOperation(string number) //this method takes in a string an does math 
+        {
+            return (Convert.ToInt32(number) - 20);
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(); //instantiation 
+            
+            Console.WriteLine(car.myMathOperation(8)); //int method
+            Console.WriteLine(car.myMathOperation(2.5m)); //decimal method 
+            Console.WriteLine(car.myMathOperation("2")); //string method
+        }
+    }
+}
