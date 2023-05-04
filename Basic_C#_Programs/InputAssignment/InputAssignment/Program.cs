ceaeea06fb6a4b9515b63d6d0c6bdb100967229c
Pablo <psalazar5@live.com>
2023-05-04 18:02:39 -0400

    Inputassignment

---
+++ Basic_C#_Programs/InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //prompting user to enter number 
+            Console.WriteLine("Hello, pick a number please: ");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            //opening file in writer mode and writing the user input to it , logging to a text file 
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\psala\OneDrive\Desktop\The-Tech-Academy-Basic-C-Sharp-Projects\Basic_C#_Programs\number.txt", true))
+            {
+                sw.WriteLine(answer);
+            }
+            //prints the numbers back to user 
+            using (StreamReader SR = new StreamReader(@"C:\Users\psala\OneDrive\Desktop\The-Tech-Academy-Basic-C-Sharp-Projects\Basic_C#_Programs\number.txt", true))
+            {
+                Console.WriteLine("Here is the number you entered: " + SR.ReadToEnd());
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -11,37 +12,32 @@ namespace TwentyOne
         static void Main(string[] args)
         {
 
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Lets start by telling me your name.");
+            string playerName = Console.ReadLine();
 
-            Deck deck = new Deck();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
 
-            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
 
-            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+            if (answer == "yes" || answer == "yea" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while(player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
 
-            List<int> numberList = new List<int>() { 1, 2, 3, 535, 342, 23 };
-
-            int sum = numberList.Where(x => x > 20).Sum();
-            
-
-            Console.WriteLine(sum);
-            
-
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
         }
         
     }
 }
-
-            //Card card1 = new Card();
-            //Card card2 = card1;
-            //card1.Face = Face.Eight;
-            //card2.Face = Face.King;
-
-            //Console.WriteLine(card1.Face);
