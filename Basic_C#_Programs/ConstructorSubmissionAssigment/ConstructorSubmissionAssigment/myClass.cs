42c0f95120606aa1719e6b432c6fd1edbb40931a
Pablo <psalazar5@live.com>
2023-05-05 13:10:42 -0400

    constructorsubmission

---
+++ Basic_C#_Programs/ConstructorSubmissionAssigment/ConstructorSubmissionAssigment/myClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmissionAssigment
+{
+    class myClass
+    { //two props
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        //default constructor calls parameterized constructor
+        public myClass() : this("idk", 0)
+        {
+
+        }
+
+        //Parameterized constructor 
+        public myClass(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+    }
+}
