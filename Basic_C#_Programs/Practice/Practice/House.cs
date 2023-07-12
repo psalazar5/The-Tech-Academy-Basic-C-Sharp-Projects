3d3bfb870d0859f181767386963502ab3748e097
Pablo <psalazar5@live.com>
2023-07-12 01:25:00 -0400

    ConsoleApp
    Console application for home intentions.

---
+++ Basic_C#_Programs/Practice/Practice/House.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    //Writing a house class the year it was built and the size to initialize object 
+    public class House
+    {
+        //filled the constructor arguments into properties and made sure the object held the data using the debugger
+        public int yearBuilt { get; set; }//instead of private int yearBuilt;
+        public string size { get; set; } //instead of private string size;
+
+        public House(int yearBuilt, string size)
+        {
+            this.yearBuilt = yearBuilt;
+            this.size = size;
+        }
+
+        public void DisplayDetails(string houseName)
+        {
+            Console.WriteLine($"{houseName} was built in: {yearBuilt}");
+            Console.WriteLine($"The size of {houseName} is : {size}");
+            Console.WriteLine($"Can the house be sold within 15 years of owner ship? {CanBeSold()}");
+        }
+         private int HowOld()
+         {
+             int currentYear = DateTime.Now.Year;
+             return currentYear - yearBuilt;
+         }
+        public bool CanBeSold()
+        {
+            int age = HowOld();
+            return age > 15; 
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/Practice/Practice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("House 1 : ");
+            Console.WriteLine("Enter the year the house was built: ");
+
+            int yearBuilt1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the size of the house: ");
+            string size1 = Console.ReadLine();
+
+            House house = new House(yearBuilt1, size1);
+
+            Console.WriteLine("\nHouse #2 details:");
+            Console.WriteLine("Enter the year the house was bullt:");
+
+            int yearbuilt2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the size of the house:");
+            string size2 = Console.ReadLine();
+
+            House house2 = new House(yearbuilt2, size2);
+
+            house.DisplayDetails("\nHouse 1");
+
+            house2.DisplayDetails("\nHouse 2");
+            Console.ReadLine();
+        }     
+    }
+}
+
+
+
+
